9ad679f50e71e24dec60ba781c7f99d35364c4b0
Jesus-1996 <38133794+koussaykhelil@users.noreply.github.com>
2019-10-21 11:26:39 +0100

    Add files via upload

---
+++ PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public  float speed = 30; 
+    public string axe; 
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float axis = Input.GetAxisRaw(axe) * speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, axis);
+    }
+}
